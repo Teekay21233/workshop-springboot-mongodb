0cc2805e1df8ffcaf63265fc6338b5d8dfd1cf95
Gustavo Duarte <gustavoduarte.21233@gmail.com>
2025-09-27 14:18:07 -0300

    Entity User and REST working

---
+++ src/main/java/com/nelioalves/workshop_springboot_mongodb/domain/User.java
@@ -0,0 +1,62 @@
+package com.nelioalves.workshop_springboot_mongodb.domain;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+//    private List<Post> posts = new ArrayList<>();
+
+    public User(){}
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public final boolean equals(Object object) {
+        if (!(object instanceof User user)) return false;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
