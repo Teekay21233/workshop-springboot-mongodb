2dcfcf4def2add00d8c8787eb752c4fcce6b565a
Gustavo Duarte <gustavoduarte.21233@gmail.com>
2025-09-27 18:12:13 -0300

    User findById

--- src/main/java/com/nelioalves/workshop_springboot_mongodb/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshop_springboot_mongodb/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.nelioalves.workshop_springboot_mongodb.dto.UserDto;
 import com.nelioalves.workshop_springboot_mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,12 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).toList();
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDto> findById(@PathVariable String id){
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDto(obj));
     }
 }
--- src/main/java/com/nelioalves/workshop_springboot_mongodb/services/UserService.java
+++ src/main/java/com/nelioalves/workshop_springboot_mongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.nelioalves.workshop_springboot_mongodb.services;
 
 import com.nelioalves.workshop_springboot_mongodb.domain.User;
 import com.nelioalves.workshop_springboot_mongodb.repositories.UserRepository;
+import com.nelioalves.workshop_springboot_mongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
 }
