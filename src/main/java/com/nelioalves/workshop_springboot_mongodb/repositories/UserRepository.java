1b11519671933b12bb7ae4e0075777deab0b5f21
Gustavo Duarte <gustavoduarte.21233@gmail.com>
2025-09-27 16:11:55 -0300

    Connecting MongoDB with repository and service

---
+++ src/main/java/com/nelioalves/workshop_springboot_mongodb/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nelioalves.workshop_springboot_mongodb.repositories;
+
+import com.nelioalves.workshop_springboot_mongodb.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+
+}
