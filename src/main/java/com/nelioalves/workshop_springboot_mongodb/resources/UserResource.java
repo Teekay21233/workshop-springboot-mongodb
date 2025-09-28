4abd4f156a9a3371fb2194b1297f54b144c292f0
Gustavo Duarte <gustavoduarte.21233@gmail.com>
2025-09-28 13:22:49 -0300

    User update using PUT

--- src/main/java/com/nelioalves/workshop_springboot_mongodb/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshop_springboot_mongodb/resources/UserResource.java
@@ -41,6 +41,15 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<UserDto> update(@RequestBody UserDto objDto, @PathVariable String id){
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+
+        return ResponseEntity.noContent().build();
+    }
+
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<UserDto> insert(@RequestBody UserDto objDto){
         User obj = service.fromDTO(objDto);
