cce418ff0b4a8b0ee65d09ca61bd0ea2b6624d73
Gustavo Duarte <gustavoduarte.21233@gmail.com>
2025-09-28 14:23:02 -0300

    Reference user's posts

--- src/main/java/com/nelioalves/workshop_springboot_mongodb/domain/User.java
+++ src/main/java/com/nelioalves/workshop_springboot_mongodb/domain/User.java
@@ -1,10 +1,12 @@
 package com.nelioalves.workshop_springboot_mongodb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -19,7 +21,8 @@ public class User implements Serializable {
     private String name;
     private String email;
 
-//    private List<Post> posts = new ArrayList<>();
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(){}
 
@@ -51,6 +54,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
