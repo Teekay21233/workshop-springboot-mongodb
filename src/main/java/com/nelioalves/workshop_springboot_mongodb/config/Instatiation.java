9581fe0c5c6a521f924bc6f29bdc274d5e99a5b5
Gustavo Duarte <gustavoduarte.21233@gmail.com>
2025-09-28 14:02:51 -0300

    Create Post entity with embedded User

--- src/main/java/com/nelioalves/workshop_springboot_mongodb/config/Instatiation.java
+++ src/main/java/com/nelioalves/workshop_springboot_mongodb/config/Instatiation.java
@@ -1,11 +1,15 @@
 package com.nelioalves.workshop_springboot_mongodb.config;
 
+import com.nelioalves.workshop_springboot_mongodb.domain.Post;
 import com.nelioalves.workshop_springboot_mongodb.domain.User;
+import com.nelioalves.workshop_springboot_mongodb.repositories.PostRepository;
 import com.nelioalves.workshop_springboot_mongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 @Configuration
@@ -14,14 +18,25 @@ public class Instatiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, LocalDate.parse("21/03/2018",fmt), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",maria);
+        Post post2 = new Post(null, LocalDate.parse("23/03/2018",fmt), "Bom dia", "Acordei feliz hoje!",maria);
+
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
---
+++ src/main/java/com/nelioalves/workshop_springboot_mongodb/domain/Post.java
@@ -0,0 +1,88 @@
+package com.nelioalves.workshop_springboot_mongodb.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Document(collection = "post")
+public class Post implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private LocalDate date;
+    private String title;
+    private String body;
+
+    private User author;
+
+    public Post(){}
+
+    public Post(String id, LocalDate date, String title, String body, User author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+
+    @Override
+    public final boolean equals(Object object) {
+        if (!(object instanceof Post post)) return false;
+
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
